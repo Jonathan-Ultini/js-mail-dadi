eaacdb32314f2cd260897d4522bc56d2e2900024
Jonathan-Ultini <jonathanultini@gmail.com>
2024-08-19 15:49:47 +0200

    esercizio 2

--- script.js
+++ script.js
@@ -26,5 +26,43 @@ lanciaDadi.addEventListener('click', function () {
   }
 
   // Stampa il risultato in pagina
-  dadiRisultato.textContent = `Giocatore: ${numeroGiocatore}, Computer: ${numeroComputer} - ${risultato}`;
+  dadiRisultato.innerHTML = `Giocatore: ${numeroGiocatore}, Computer: ${numeroComputer} - ${risultato}`;
+});
+
+// ESERCIZIO 2 - Finta Login
+
+// Lista di email autorizzate
+const emailAutorizzate = [
+  "mario@rossi.com",
+  "marco@bianchi.com",
+  "gianni@manni.com",
+  "danny@kanny.com",
+  "pinco@pallino.com"
+];
+
+// Seleziona gli elementi del DOM
+const emailInput = document.getElementById('emailInput');
+const verificaEmail = document.getElementById('verificaEmail');
+const esitoLogin = document.getElementById('esitoLogin');
+
+// Aggiunge l'evento al click del bottone per verificare l'email
+verificaEmail.addEventListener('click', function () {
+  // Ottiene il valore dell'input
+  const emailUtente = emailInput.value;
+  let emailTrovata = false;
+
+  // Controlla se l'email è nella lista autorizzata
+  for (let i = 0; i < emailAutorizzate.length; i++) {
+    if (emailUtente === emailAutorizzate[i]) {
+      emailTrovata = true;
+      break;
+    }
+  }
+
+  // Stampa il risultato del controllo in pagina
+  if (emailTrovata) {
+    esitoLogin.innerHTML = "Accesso consentito.";
+  } else {
+    esitoLogin.innerHTML = "Accesso negato.";
+  }
 });
